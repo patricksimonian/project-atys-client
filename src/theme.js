75abff1344ab176d82a775a3d0a488d2bb5f367d
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-12-08 18:37:15 -0800

    prettified

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,20 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: '#635973',
-      main: '#3d3051',
-      dark: '#2a2138',
-      contrastText: '#fff',
+      light: "#635973",
+      main: "#3d3051",
+      dark: "#2a2138",
+      contrastText: "#fff"
     },
     secondary: {
-      light: '#454992',
-      main: '#6369d1',
-      dark: '#8287da',
-      contrastText: '#fff',
-    },
-  },
+      light: "#454992",
+      main: "#6369d1",
+      dark: "#8287da",
+      contrastText: "#fff"
+    }
+  }
 });
 
 export default theme;

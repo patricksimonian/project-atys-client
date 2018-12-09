9e666682fde1b94920d216a14b06f913d43abf55
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-12-08 19:04:49 -0800

    prettify

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,22 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 const styles = {
   card: {
-    maxWidth: 345
+    maxWidth: 345,
   },
   media: {
     // ⚠️ object-fit is not supported by IE 11.
-    objectFit: "cover"
-  }
+    objectFit: 'cover',
+  },
 };
 
 function ImgMediaCard(props) {
@@ -37,8 +37,8 @@ function ImgMediaCard(props) {
             Lizard
           </Typography>
           <Typography component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
           </Typography>
         </CardContent>
       </CardActionArea>
@@ -55,7 +55,7 @@ function ImgMediaCard(props) {
 }
 
 ImgMediaCard.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(ImgMediaCard);
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,13 @@
-import React, { Component } from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { MuiThemeProvider } from "@material-ui/core/styles";
-import theme from "../theme";
-import { withStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import AppBar from "../components/AppBar/AppBar";
-import Drawer from "../components/Drawer/Drawer";
-import Card from "../components/Card/Card";
+import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import theme from '../theme';
+import Grid from '@material-ui/core/Grid';
+import AppBar from '../components/AppBar/AppBar';
+import Drawer from '../components/Drawer/Drawer';
+import Card from '../components/Card/Card';
 
-import "./App.css";
+import './App.css';
 
 class App extends Component {
   render() {
